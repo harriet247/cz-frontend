91148eeb5850a91c7cb0cf9b4e966ef9f2222124
HongxiG <r3j2b@ugrad.cs.ubc.ca>
2024-04-15 12:27:48 -0700

    fine tune the position of color tag

--- src/Item.js
+++ src/Item.js
@@ -39,6 +39,15 @@ const useStyle = makeStyles(theme => ({
         fontSize: 14,
         marginTop: 1,
     },
+    discount:{
+        color: '#B35C1E',
+        ontSize: '14px',
+        whiteSpace: 'nowrap',
+        marginRight: 5,
+        fontFamily: "Campton-SemiBold",
+        fontSize: 14,
+        marginTop: 1,
+    },
     customize: {
         display: 'flex',
         cursor: 'pointer',
@@ -50,21 +59,21 @@ const useStyle = makeStyles(theme => ({
         alignItems: "center"
     },
     tag: {
-        position: 'absolute',
-        bottom: 70,
+        position: "absolute",
+        bottom: "24px",
         zIndex: 999,
         right: 10,
-        [theme.breakpoints.down('sm')]: {
-            right: 25,
+        [theme.breakpoints.down("sm")]: {
+          right: 25,
         },
-        display: 'flex',
-        alignItems: 'center',
-        padding: '6px 6px 6px 10px',
-        gap: '6px',
-        backgroundColor: '#fff',
-        border: '0.5px solid #D0CEC4',
-        borderRadius: '100px',
-        cursor: 'pointer',
+        display: "flex",
+        alignItems: "center",
+        padding: "6px 6px 6px 10px",
+        gap: "8px",
+        backgroundColor: "#fff",
+        border: "0.5px solid #D0CEC4",
+        borderRadius: "100px",
+        cursor: "pointer",
     },
 }))
 function Item({ img, tag, tagColor, name, price, discount }) {
@@ -81,33 +90,41 @@ function Item({ img, tag, tagColor, name, price, discount }) {
                     justifyContent: 'center',
                     flexDirection: 'column',
                     alignItems: 'center',
+                    width: "100%",
+    height: "100%",
+    gap: "6px"
                 }}>
-                <img src={img} alt="" />
-                <Box className={classes.tag} style={{alignItems:"center"}}>
-                    <span style={{ fontFamily: 'Campton-Medium', fontSize:12, color: "#4F6076"}}>{tag}</span>
-                    <Box
-                        style={{
-                            backgroundColor: tagColor,
-                            borderRadius: '100%',
-                            width: '20px',
-                            height: '20px',
-                        }}></Box>
+                <Box style={{width: "100%",height: "100%",position:"relative"}}>
+                    <img src={img} alt=""/>
+                    <Box className={classes.tag} style={{alignItems:"flex-end", display:"flex"}}>
+                        <span style={{ fontFamily: 'Campton-Medium', fontSize:12, color: "#4F6076"}}>{tag}</span>
+                        <Box
+                            style={{
+                                backgroundColor: tagColor,
+                                borderRadius: '100%',
+                                width: '20px',
+                                height: '20px',
+                            }}></Box>
+                    </Box>
                 </Box>
-
                 <Box className={classes.text}>
-                    <Box>
-                        <strong style={{ fontFamily: 'Campton-SemiBold' }}>
+                    <Box >
+                        <strong style={{ fontFamily: 'Campton-Medium', fontSize: 15}}>
                            {name}
                         </strong>
                         <Box style={{ display: 'flex',alignItems: "center"}}>
                             {discount === undefined? 
-                            <span className={classes.money}>{price} or financing |</span> : 
-                            <span className={classes.money}>{price} | Save {discount}</span>}
+                            <><span className={classes.money}>{price} or financing |</span> 
                             <span className={classes.customize}>
                                 Customize
                                 <ArrowForward fontSize='small' style={{marginLeft:3}}/>
-                            </span>
+                            </span></>: 
+                            <span className={classes.money}>{price} | <span className={classes.discount}>Save {discount}</span></span>}
                         </Box>
+                        {discount===undefined?<></>:<span className={classes.customize}>
+                                Customize
+                                <ArrowForward fontSize='small' style={{marginLeft:3}}/>
+                            </span>}
                     </Box>
                 </Box>
             </Box>
